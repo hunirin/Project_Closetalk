9386253aa65a13a2ec5b8694c60fbe75330e85a1
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-14 11:33:17 +0900

    CLTK-13: 기본 MVC 구조 생성

---
+++ src/main/java/team/closetalk/closet/service/ClosetService.java
@@ -0,0 +1,9 @@
+package team.closetalk.closet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClosetService {
+}
