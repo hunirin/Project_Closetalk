30c5de32e610132130ea94d5ef5e1f3e86d005e7
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-17 16:28:27 +0900

    CLTK-20 : 가지고 있는 옷장 목록 조회 (POST /closet)

---
+++ src/main/java/team/closetalk/closet/dto/ClosetDto.java
@@ -0,0 +1,23 @@
+package team.closetalk.closet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import team.closetalk.closet.entity.ClosetEntity;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClosetDto {
+    private String closetName;
+    private Boolean isHidden;
+
+    public static ClosetDto viewCloset(ClosetEntity closet) {
+        return ClosetDto.builder()
+                .closetName(closet.getClosetName())
+                .isHidden(closet.getIsHidden())
+                .build();
+    }
+}
