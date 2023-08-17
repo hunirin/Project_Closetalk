3b0c220c5527a9c1d85cd262ae6c58986798b75b
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-17 17:27:01 +0900

    CLTK-22 : 옷장 추가 1. 옷장 최대 생성 5개까지 제한 2. closetName : Key or Value == Null -> My Closet [마지막으로 생성된 ClosetId + 1] 순으로 생성 3. isHidden : default value -> false

--- src/main/java/team/closetalk/closet/controller/ClosetController.java
+++ src/main/java/team/closetalk/closet/controller/ClosetController.java
@@ -14,15 +14,28 @@ import java.util.List;
 public class ClosetController {
     private final ClosetService closetService;
 
+    // 옷장 목록
     @PostMapping
-    public List<ClosetDto> readAllCloset() {
-        return closetService.readCloset();
+    public List<ClosetDto> closetList() {
+        return closetService.findCloset();
     }
 
+    // 옷장 생성
+    @PostMapping("/create")
+    public void closetCreate(@RequestParam(name = "closetName", required = false)
+                                 String closetName,
+                             @RequestParam(name = "isHidden", required = false,
+                                     defaultValue = "false") Boolean isHidden) {
+        closetService.addCloset(closetName, isHidden);
+    }
+
+    // 해당 옷장 아이템 목록 조회
     @PostMapping("/{closetId}")
     public List<ClosetItemDto> readItems(@PathVariable("closetId") Long closetId,
                                          @RequestParam(name = "category", required = false) String category) {
+        // 카테고리 입력이 없을 시 전체 목록
         if (category == null) return closetService.readByCloset(closetId);
+        // 있으면 카테고리 별 목록
         else return closetService.readByCategory(closetId, category);
     }
 }
