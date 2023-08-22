59ab567e5f6b614f55d5956424f2938f0f3eb3c1
youngseobkim <ks_ym0522@naver.com>
2023-08-22 14:39:22 +0900

    CLTK-25 : 조회수 증가

--- CLTK-25/src/main/java/team/closetalk/repository/IssueArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package team.closetalk.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.closetalk.entity.IssueArticleEntity;
-
-public interface IssueArticleRepository extends JpaRepository<IssueArticleEntity, Long> {
-}
