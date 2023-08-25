b538b46d1af0ddd2fdc3333b847311ab7de1d6e0
bluelily555 <bluelily555@daum.net>
2023-08-25 17:11:31 +0900

    CLTK-35: 회원가입 폼 email 입력 관련 부분 수정 및 로그인, 회원 관련 주석 해제

--- src/main/java/team/closetalk/user/config/WebSecurityConfig.java
+++ src/main/java/team/closetalk/user/config/WebSecurityConfig.java
@@ -9,64 +9,52 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
-//import team.closetalk.user.service.CustomOAuth2UserService;
+import team.closetalk.user.service.CustomOAuth2UserService;
 import team.closetalk.user.utils.JwtFilter;
-//import team.closetalk.user.utils.OAuth2SuccessHandler;
+import team.closetalk.user.utils.OAuth2SuccessHandler;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final JwtFilter jwtFilter;
-//    private final CustomOAuth2UserService customOAuth2UserService;
-//    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer :: disable)
                 .authorizeHttpRequests(
-                        authHttp -> authHttp.requestMatchers(
-                                "/"
-                                ,"/testPage"
-                                , "/js/join-form.js"
-                                ,"/issue/**"
-//                                "/main"
-//                                , "relayAuth"
-                        ).permitAll()
-                        .requestMatchers(
-                                "/joinPage"
-                                , "/users/sendEmail"
-                                , "/users/register"
-                                , "/users/login"
-                        ).anonymous()
-                        .requestMatchers(
-                                "/issue/create"
-//                                "/myPage"
-////                                , "/users/userInfo"
-////                                , "/authPage"
-                        ).authenticated()
-
-                )
-                //폼로그인 추가
-                .formLogin( formHttp -> formHttp.loginPage("/loginPage")
-                        .defaultSuccessUrl("/loginPage")
-                        .failureUrl("/loginPage")
-                        .permitAll()
+                        authHttp -> authHttp
+                                .requestMatchers(
+                                        "/joinPage"
+                                        , "/users/sendEmail"
+                                        , "/users/register"
+                                        , "/users/login"
+                                        , "users/login-token"
+                                        ,"/ootd/main"
+                                        ,"/ootd/header"
+                                        ,"/ootd/list"
+                                        ,"/static/**"
+                                        ,"/loginPage"
+                                ).permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
                 //소셜로그인 추가
-//                .oauth2Login( oauthHttp -> oauthHttp.loginPage("/loginPage")
-//                        .successHandler(oAuth2SuccessHandler)
-//                        .failureUrl("/loginPage")
-//                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-//                        .permitAll()
-//                )
+                .oauth2Login( oauthHttp -> oauthHttp.loginPage("/loginPage")
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureUrl("/loginPage")
+                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
+                        .permitAll()
+                )
                 .sessionManagement(
                         sessionManagement -> sessionManagement.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS
                         )
                 ).addFilterBefore(
                         jwtFilter
-                , AuthorizationFilter.class
+                        , AuthorizationFilter.class
                 );
 
         return http.build();
--- src/main/java/team/closetalk/user/service/EmailSendService.java
+++ src/main/java/team/closetalk/user/service/EmailSendService.java
@@ -1,57 +1,57 @@
-//package team.closetalk.user.service;
-//
-//import jakarta.mail.Message;
-//import jakarta.mail.MessagingException;
-//import jakarta.mail.internet.InternetAddress;
-//import jakarta.mail.internet.MimeMessage;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//import java.io.UnsupportedEncodingException;
-//import java.util.Random;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class EmailSendService {
-//    private final JavaMailSender javaMailSender;
-//    private static final String ADMIN_ADDRESS = "xhxh006@naver.com";
-//
-//    private int authCode;
-//
-//    public void makeAuthCode(){
-//        Random random = new Random();
-//        authCode = random.nextInt(888888) + 111111;
-//    }
-//
-//    public String makeEmailAuth(String receiver){
-//        makeAuthCode();
-//
-//        String title = "[CloseTalk] 클로젯톡 회원가입을 위한 인증 메일입니다.";
-//
-//        String content = "귀하의 회원가입을 위한 이메일 인증번호입니다.\n"
-//                +authCode;
-//
-//        sendEmail(receiver, title, content);
-//
-//        return Integer.toString(authCode);
-//
-//    }
-//
-//    public void sendEmail(String receiver, String title, String content){
-//        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-//
-//        try{
-//            mimeMessage.addRecipients(Message.RecipientType.TO, receiver);
-//            mimeMessage.setSubject(title);
-//            mimeMessage.setText(content);
-//            mimeMessage.setFrom(new InternetAddress(ADMIN_ADDRESS, "CLOSETALK"));
-//            javaMailSender.send(mimeMessage);
-//        } catch (MessagingException e){
-//            throw new RuntimeException(e);
-//        } catch (UnsupportedEncodingException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//}
+package team.closetalk.user.service;
+
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class EmailSendService {
+    private final JavaMailSender javaMailSender;
+    private static final String ADMIN_ADDRESS = "xhxh006@naver.com";
+
+    private int authCode;
+
+    public void makeAuthCode(){
+        Random random = new Random();
+        authCode = random.nextInt(888888) + 111111;
+    }
+
+    public String makeEmailAuth(String receiver){
+        makeAuthCode();
+
+        String title = "[CloseTalk] 클로젯톡 회원가입을 위한 인증 메일입니다.";
+
+        String content = "귀하의 회원가입을 위한 이메일 인증번호입니다.\n"
+                +authCode;
+
+        sendEmail(receiver, title, content);
+
+        return Integer.toString(authCode);
+
+    }
+
+    public void sendEmail(String receiver, String title, String content){
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try{
+            mimeMessage.addRecipients(Message.RecipientType.TO, receiver);
+            mimeMessage.setSubject(title);
+            mimeMessage.setText(content);
+            mimeMessage.setFrom(new InternetAddress(ADMIN_ADDRESS, "CLOSETALK"));
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e){
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
