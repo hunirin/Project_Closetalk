11bc53e1ad030b8b2b3e73141b66dea0a691a617
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-28 02:47:18 +0900

    CLTK-35 : nickname으로 user 찾기 추가

--- src/main/java/team/closetalk/user/repository/UserRepository.java
+++ src/main/java/team/closetalk/user/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
 
     Optional<UserEntity> findByLoginId(String username);
+
+    Optional<UserEntity> findByNickname(String nickname);
 }
