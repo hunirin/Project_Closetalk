45cc9fbf4da1d395c1901210317f34fecb93d51b
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-08-30 14:43:52 +0900

    CLTK-4 : CommunityArticleEntity 수정 - LocalDate -> LocalDateTime 변경 - increaseHit() : 조회수 증가 - deleteArticle() : 게시글 삭제(Soft Delete) - CommunityArticleEntity() : 게시글 생성(아직 사용 안하고 있음) - 그 외 Column 관련 name, nullable, default value 추가

--- src/main/java/team/closetalk/community/entity/CommunityArticleEntity.java
+++ src/main/java/team/closetalk/community/entity/CommunityArticleEntity.java
@@ -2,36 +2,63 @@ package team.closetalk.community.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+import org.hibernate.annotations.ColumnDefault;
 import team.closetalk.user.entity.UserEntity;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Data
 @Entity
 @Table(name = "communityArticle")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommunityArticleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String category;    // 카테고리
+    @Column(nullable = false)
     private String title;       // 제목
+    @Column(nullable = false)
     private String content;     // 내용
+    @ColumnDefault(value = "0")
     private Long hits;          // 조회수
     private String thumbnail;   // 대표이미지
-    private LocalDate createdAt;    // 작성 날짜
-    private LocalDate modifiedAt;    // 수정 날짜
-
-    @OneToMany(mappedBy = "communityArticle")
-    private List<CommunityArticleImagesEntity> communityImages = new ArrayList<>();
-
-    @OneToMany(mappedBy = "communityArticle")
-    private List<CommunityCommentEntity> communityComments = new ArrayList<>();
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    private UserEntity user;
+    private UserEntity userId;
+
+    public CommunityArticleEntity(String category, String title,
+                                  String content, UserEntity user) {
+        this.category = category;
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        this.userId = user;
+    }
+
+    // 조회수 증가
+    public CommunityArticleEntity increaseHit() {
+        this.hits = hits + 1;
+        return this;
+    }
+
+    // 게시글 삭제
+    public CommunityArticleEntity deleteArticle() {
+        this.deletedAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        return this;
+    }
 }
