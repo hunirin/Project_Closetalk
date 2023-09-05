8ebdff48fa8161ef4dcd3d2644d3ccd3dc0ec65e
youngseobkim <ks_ym0522@naver.com>
2023-09-06 01:02:14 +0900

    CLTK-57 : CommunityLikeController 게시글 좋아요 메서드 추가

--- src/main/java/team/closetalk/community/controller/CommunityArticleController.java
+++ src/main/java/team/closetalk/community/controller/CommunityArticleController.java
@@ -2,14 +2,18 @@ package team.closetalk.community.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import team.closetalk.community.dto.CommunityArticleDto;
 import team.closetalk.community.dto.CommunityArticleListDto;
 import team.closetalk.community.dto.CommunityCreateArticleDto;
+import team.closetalk.community.entity.CommunityArticleEntity;
 import team.closetalk.community.enumeration.Category;
 import team.closetalk.community.service.CommunityArticleService;
+import team.closetalk.community.service.CommunityLikeService;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +23,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CommunityArticleController {
     private final CommunityArticleService communityArticleService;
+    private final CommunityLikeService communityLikeService;
 
     // 게시글 목록 조회
     @GetMapping
@@ -76,5 +81,11 @@ public class CommunityArticleController {
             return communityArticleService.createArticleWithImages(dto, imageUrlList, authentication);
          else
              return communityArticleService.createArticle(dto, authentication);
+    }
+
+    // 게시글 좋아요
+    @PostMapping("/like/{articleId}")
+    public String likeCommunityArticle(@PathVariable Long articleId, Authentication authentication) {
+        return communityLikeService.likeCommunityArticle(articleId, authentication);
     }
 }
