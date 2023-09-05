41fbca3002325f90887c0578887df95b9bb963cd
youngseobkim <ks_ym0522@naver.com>
2023-09-05 22:40:58 +0900

    CLTK-57 : CommunityLikeRepository Id부분 수정

--- src/main/java/team/closetalk/community/repository/CommunityLikeRepository.java
+++ src/main/java/team/closetalk/community/repository/CommunityLikeRepository.java
@@ -1,15 +1,15 @@
 package team.closetalk.community.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import team.closetalk.community.entity.CommunityArticleEntity;
 import team.closetalk.community.entity.CommunityLikeEntity;
+import team.closetalk.user.entity.UserEntity;
 
 import java.util.Optional;
 
+
 public interface CommunityLikeRepository extends JpaRepository<CommunityLikeEntity, Long> {
 
-    Optional<CommunityLikeEntity> findByUserIdAndArticleId(Long userId, Long articleId);
-
+//    Optional<CommunityLikeEntity> findByUserIdAndArticleId(Long userId, Long articleId);
+    Optional<CommunityLikeEntity> findByUserIdIdAndArticleIdId(Long userId, Long articleId);
 }
