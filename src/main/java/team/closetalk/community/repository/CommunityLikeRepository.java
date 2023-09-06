ef6cfbad0e42e95b4fdde983b7563202856938ae
youngseobkim <ks_ym0522@naver.com>
2023-09-06 13:44:53 +0900

    CLTK-57 : CommunityLikeRepository 좋아요 수 체크 위한 count 쿼리 메서드 추가

--- src/main/java/team/closetalk/community/repository/CommunityLikeRepository.java
+++ src/main/java/team/closetalk/community/repository/CommunityLikeRepository.java
@@ -1,15 +1,13 @@
 package team.closetalk.community.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import team.closetalk.community.entity.CommunityArticleEntity;
 import team.closetalk.community.entity.CommunityLikeEntity;
-import team.closetalk.user.entity.UserEntity;
 
 import java.util.Optional;
 
 
 public interface CommunityLikeRepository extends JpaRepository<CommunityLikeEntity, Long> {
-
-//    Optional<CommunityLikeEntity> findByUserIdAndArticleId(Long userId, Long articleId);
     Optional<CommunityLikeEntity> findByUserIdIdAndArticleIdId(Long userId, Long articleId);
+
+    Long countByArticleIdId(Long articleId);
 }
