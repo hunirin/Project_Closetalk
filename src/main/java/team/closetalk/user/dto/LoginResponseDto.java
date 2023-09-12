1df0584b2a6df0147cfd4f01bc9f56c7de1d69fe
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-09-12 09:13:39 +0900

    CLTK-2-dev : Jwt -> access, refresh 분리 - JwtTokenDto -> LoginRequestDto, LoginResponseDto 분리 - 로그인 부분 수정: OAuth2SuccessHandler(소셜), UserController(일반 로그인)

---
+++ src/main/java/team/closetalk/user/dto/LoginResponseDto.java
@@ -0,0 +1,13 @@
+package team.closetalk.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private String nickname;
+    private String accessToken;
+}
