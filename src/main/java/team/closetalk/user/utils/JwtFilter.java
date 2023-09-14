83f9e16ac7270e25ea78d240d62c2e6a054b1a92
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-09-14 11:13:18 +0900

    CLTK-2-dev : redis - refreshtoken으로 accesstoken 재발급

--- src/main/java/team/closetalk/user/utils/JwtFilter.java
+++ src/main/java/team/closetalk/user/utils/JwtFilter.java
@@ -11,10 +11,12 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import team.closetalk.user.dto.CustomUserDetails;
 import team.closetalk.user.service.TokenService;
+import team.closetalk.user.service.UserService;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,6 +26,7 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtils jwtUtils;
+    private final UserService userService;
     private final TokenService tokenService;
 
     @Override
@@ -32,36 +35,47 @@ public class JwtFilter extends OncePerRequestFilter {
 
         if(authHeader != null && authHeader.startsWith("Bearer ")){
             String token = authHeader.split(" ")[1];
-            if(jwtUtils.validate(token)){
-                if (isTokenValidInRedis(token)) {
-                    SecurityContext context = SecurityContextHolder.createEmptyContext();
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
 
-                    String username = jwtUtils.parseClaims(token).getSubject();
+            if(jwtUtils.validate(token)){
+                log.info("토큰 검증 완료");
+                String username = jwtUtils.parseClaims(token).getSubject();
+                AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        CustomUserDetails.builder()
+                                .loginId(username)
+                                .build()
+                        , token
+                        , new ArrayList<>()
+                );
+                context.setAuthentication(authenticationToken);
+                SecurityContextHolder.setContext(context);
+            } else {
+                log.info("AccessToken 만료 : Redis - RefreshToken 탐색");
+                String username = jwtUtils.extractUsernameFromExpiredToken(token);
+                String refreshToken = tokenService.getRefreshToken(username);
+                if (jwtUtils.validate(refreshToken)) {
+                    log.info("새로운 AccessToken 발급");
+                    UserDetails responseUser = userService.loadUserByUsername(username);
+                    String newAccessToken = jwtUtils.generateAccessToken(responseUser);
+                    String newRefreshToken = jwtUtils.generateRefreshToken(responseUser);
+                    tokenService.saveRefreshToken(newRefreshToken, username);
+
                     AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                             CustomUserDetails.builder()
                                     .loginId(username)
-                                    .build()
-                            , token
-                            , new ArrayList<>()
+                                    .build(),
+                            newAccessToken,
+                            new ArrayList<>()
                     );
-
                     context.setAuthentication(authenticationToken);
                     SecurityContextHolder.setContext(context);
+                    response.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+                    response.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + newAccessToken);
                 } else {
-                    log.warn("토큰이 만료되었거나 유효하지 않습니다.");
+                    log.warn("RefreshToken이 유효하지 않습니다.");
                 }
-            } else {
-                log.warn("인증되지 않은 사용자입니다.");
             }
         }
         filterChain.doFilter(request, response);
-    }
-
-    private boolean isTokenValidInRedis(String token) {
-        String username = jwtUtils.parseClaims(token).getSubject();
-        String storedToken = tokenService.getAccessToken(username);
-
-        // Redis에서 저장된 토큰과 비교
-        return storedToken != null && storedToken.equals(token);
     }
 }
