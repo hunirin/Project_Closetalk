31d192df88deae450855e484dd5c5edff3886ca7
youngseobkim <ks_ym0522@naver.com>
2023-09-14 10:07:30 +0900

    CLTK-84 : ISSUE 게시글 작성

--- src/main/java/team/closetalk/issue/controller/IssueViewController.java
+++ src/main/java/team/closetalk/issue/controller/IssueViewController.java
@@ -1,16 +1,20 @@
 package team.closetalk.issue.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import team.closetalk.issue.dto.IssueArticleDto;
 import team.closetalk.issue.dto.IssueArticleListDto;
+import team.closetalk.issue.dto.IssueCreateArticleDto;
 import team.closetalk.issue.enumeration.Category;
 import team.closetalk.issue.service.IssueArticleSearchService;
 import team.closetalk.issue.service.IssueArticleService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/issue/view")
@@ -23,7 +27,7 @@ public class IssueViewController {
     public String getIssues(
             @RequestParam(value = "category", defaultValue = "ALL") Category category,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+            @RequestParam(value = "limit", defaultValue = "8") Integer limit,
             Model model) {
 
         Page<IssueArticleListDto> issueList;
@@ -50,7 +54,7 @@ public class IssueViewController {
             @RequestParam(value = "searchType", defaultValue = "all") String searchType,
             @RequestParam(value = "keyword", required = false) String keyword,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+            @RequestParam(value = "limit", defaultValue = "8") Integer limit,
             Model model
     ) {
         Page<IssueArticleListDto> issueList;
@@ -80,5 +84,29 @@ public class IssueViewController {
         model.addAttribute("articleId", articleId);
 
         return "issue/issueArticle";
+    }
+    @GetMapping("/create")
+    public String CreateArticleForm() {
+        return "issue/issueCreate";
+    }
+
+    @PostMapping("/create")
+    public String CreateArticle(@ModelAttribute IssueCreateArticleDto dto,
+                                @RequestParam(value = "category", defaultValue = "MAGAZINE") Category category,
+                                @RequestParam(value = "imageUrlList", required = false) List<MultipartFile> imageUrlList,
+                                Authentication authentication,
+                                Model model) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            // 사용자가 인증되었는지 확인
+            IssueArticleDto issueArticle = issueArticleService.createArticle(dto, imageUrlList, authentication);
+
+            model.addAttribute("issueArticle", issueArticle);
+            model.addAttribute("category", category);
+
+            return "issue/issueList";
+        } else {
+            // 사용자가 인증되지 않은 경우, 로그인 페이지로 리디렉션 또는 오류 메시지 표시
+            return "redirect:/loginPage"; // 또는 다른 리디렉션 URL을 지정
+        }
     }
 }
