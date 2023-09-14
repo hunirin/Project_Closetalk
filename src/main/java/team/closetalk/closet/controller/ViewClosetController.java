94b6871e2a0c771edaa5943db4495660625f3014
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-09-14 11:16:33 +0900

    CLTK-2-dev : 내 옷장 endpoint 추가

---
+++ src/main/java/team/closetalk/closet/controller/ViewClosetController.java
@@ -0,0 +1,19 @@
+package team.closetalk.closet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/closet")
+@RequiredArgsConstructor
+public class ViewClosetController {
+
+    @GetMapping("/{nickname}")
+    public String closetList(@PathVariable("nickname") String nickname) {
+        return "closet/my-closet";
+    }
+}
