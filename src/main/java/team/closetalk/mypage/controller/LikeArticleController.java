77ce1ffab64b24e78ced5eb73781feb88a7c38ec
hun053@naver.com <hun053@naver.com>
2023-10-01 08:07:27 +0900

    feat: 좋아요한 글 보기 구현

--- src/main/java/team/closetalk/mypage/controller/LikeArticleController.java
+++ src/main/java/team/closetalk/mypage/controller/LikeArticleController.java
@@ -3,41 +3,60 @@ package team.closetalk.mypage.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import team.closetalk.community.dto.article.response.CommunityArticleListDto;
 import team.closetalk.mypage.service.LikedArticleService;
 import team.closetalk.ootd.dto.OotdArticleDto;
 
 
-@RestController
-@RequestMapping("/myPage")
+@Controller
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
 public class LikeArticleController {
     private final LikedArticleService likedArticleService;
 
+    @GetMapping("/liked/community")
+    public String likedCommPage() {
+        return "mypage/likedCommunityList";
+    }
+
+    @GetMapping("/liked/ootd")
+    public String likedOotdPage() {
+        return "mypage/likedOotdList";
+    }
 
     // Community
     // 좋아요 한 목록
-    @GetMapping("/liked/community")
-    public Page<CommunityArticleListDto> readCommunityArticleList(
+    @GetMapping("/liked/community/list")
+    public String readCommunityArticleList(
+            Model model,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "limit", defaultValue = "20") Integer limit,
             Authentication authentication
     ) {
-        return likedArticleService.readLikedCommunityArticlePaged(page, limit, authentication);
+        Page<CommunityArticleListDto> likedCommunityPage = likedArticleService.readLikedCommunityArticlePaged(page, limit, authentication);
+        model.addAttribute("likedCommunityPage", likedCommunityPage);
+
+        return "mypage/likedCommunityList";
+
     }
 
     // OOTD
     // 좋아요 한 목록
-    @GetMapping("/liked/ootd")
-    public Page<OotdArticleDto> readOotdArticleList(
+    @GetMapping("/liked/ootd/list")
+    public String readOotdArticleList(
+            Model model,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "limit", defaultValue = "20") Integer limit,
             Authentication authentication
     ) {
-        return likedArticleService.readLikedOotdArticlePaged(page, limit, authentication);
+        Page<OotdArticleDto> likedOotdPage = likedArticleService.readLikedOotdArticlePaged(page, limit, authentication);
+        model.addAttribute("likedOotdPage", likedOotdPage);
+
+        return "mypage/likedOotdList";
     }
 }
