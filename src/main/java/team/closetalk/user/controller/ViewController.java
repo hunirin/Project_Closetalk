77ea21fd4009b317957741ce75f2baa9d12c1ed8
hun053@naver.com <hun053@naver.com>
2023-10-03 18:04:42 +0900

    feat: header 로그인/로그아웃 추가

--- src/main/java/team/closetalk/user/controller/ViewController.java
+++ src/main/java/team/closetalk/user/controller/ViewController.java
@@ -1,15 +1,25 @@
 package team.closetalk.user.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.closetalk.user.dto.CustomUserDetails;
+import team.closetalk.user.service.UserService;
 
 @Slf4j
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class ViewController {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("loginPage")
     public String viewLoginForm(){
@@ -24,5 +34,13 @@ public class ViewController {
     @GetMapping("test")
     public String testForm() {
         return "user/test";
+    }
+
+    // 로그아웃
+    @PostMapping("logout")
+    public String logout(Authentication authentication) {
+        CustomUserDetails responseUser = userService.loadUserByUsername(authentication.getName());
+        userService.logout(responseUser);
+        return "ootd/ootdMain";
     }
 }
