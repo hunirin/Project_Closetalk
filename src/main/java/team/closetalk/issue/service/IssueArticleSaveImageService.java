38973da5c84e78a6c05eaeb94cc81f4ccb06834d
hun053@naver.com <hun053@naver.com>
2023-10-18 21:05:41 +0900

    feat: 커뮤니티 이미지 저장부분 수정

--- src/main/java/team/closetalk/issue/service/IssueArticleSaveImageService.java
+++ src/main/java/team/closetalk/issue/service/IssueArticleSaveImageService.java
@@ -14,6 +14,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -24,14 +26,15 @@ import java.util.UUID;
 public class IssueArticleSaveImageService {
     private final IssueArticleImageRepository issueArticleImageRepository;
 
-    private final String ROOT_DIRECTORY = "C:\\Users\\KYS\\git\\Project_Closetalk\\src\\main\\resources";
-    private final String ISSUE_IMAGE_DB_PATH = "\\static\\images\\issue\\";
+    private final String ROOT_DIRECTORY = "src/main/resources";
+    private final String ISSUE_IMAGE_DB_PATH = "/static/images/issue/";
 
     // 이미지 저장
     public List<IssueArticleImageEntity> saveArticleImage(IssueArticleEntity article,
                                                           List<MultipartFile> imageUrlList) {
         // 디렉토리 생성
-        String ARTICLE_IMAGE_DIRECTORY = ROOT_DIRECTORY + ISSUE_IMAGE_DB_PATH + article.getUserId().getId();
+        String ARTICLE_IMAGE_DIRECTORY = ROOT_DIRECTORY + ISSUE_IMAGE_DB_PATH + article.getId();
+        ARTICLE_IMAGE_DIRECTORY = ARTICLE_IMAGE_DIRECTORY.replace("\\", "/"); // 역슬래시를 슬래시로 치환
 
         try {
             Files.createDirectories(Path.of(ARTICLE_IMAGE_DIRECTORY));
@@ -39,7 +42,7 @@ public class IssueArticleSaveImageService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        String imagePath = ISSUE_IMAGE_DB_PATH + article.getUserId().getId();
+        String imagePath = ISSUE_IMAGE_DB_PATH + article.getId();
         // 이미지 저장용 리스트
         List<IssueArticleImageEntity> issueImages = new ArrayList<>();
 
@@ -47,32 +50,34 @@ public class IssueArticleSaveImageService {
 
             String originalFilename = image.getOriginalFilename();
 
-            if (originalFilename != null) {
-                String[] originalFilenameSplit = originalFilename.split("\\.");
-                String extension = originalFilenameSplit[originalFilenameSplit.length - 1];
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+            String timestamp = now.format(formatter);
 
-                // UUID 생성
-                String uuid = UUID.randomUUID().toString();
+            // 원본 파일 이름에서 확장자 추출
+            String originalFileName = image.getOriginalFilename();
+            String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
 
-                // 새로운 이미지 파일 이름 생성
-                String imageFileName = originalFilename.replaceFirst("\\..*", "") + "_" + uuid + "." + extension;
 
-                // 이미지 파일 경로 생성
-                String imageFilePath = Path.of(imagePath, imageFileName).toString();
+            // 새로운 이미지 파일 이름 생성
+            String imageFileName = timestamp + fileExtension;
 
-                try {
-                    image.transferTo(Path.of(ROOT_DIRECTORY + imageFilePath));
-                } catch (IOException e) {
-                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-                log.info("이미지 경로 : {}", imageFilePath);
+            // 이미지 파일 경로 생성
+            String imageFilePath = Path.of(imagePath, imageFileName).toString();
+            imageFilePath = imageFilePath.replace("\\", "/"); // 역슬래시를 슬래시로 치환
 
-                // 이미지 리스트에 저장
-                IssueArticleImageEntity savedImage = issueArticleImageRepository.save(new IssueArticleImageEntity(article, imageFilePath));
-                log.info("issueImage : {}", savedImage);
-                issueImages.add(savedImage);
-                log.info("issueImages : {}", issueImages);
+            try {
+                image.transferTo(Path.of(ROOT_DIRECTORY + imageFilePath));
+            } catch (IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
             }
+            log.info("이미지 경로 : {}", imageFilePath);
+
+            // 이미지 리스트에 저장
+            IssueArticleImageEntity savedImage = issueArticleImageRepository.save(new IssueArticleImageEntity(article, imageFilePath));
+            log.info("issueImage : {}", savedImage);
+            issueImages.add(savedImage);
+            log.info("issueImages : {}", issueImages);
         }
 
         return issueImages;
